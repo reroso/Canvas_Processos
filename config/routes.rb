a2ac705c36898f790075c7882ca8835fdacfa164
Yan  Batista <y.batista@gsuite.iff.edu.br>
2023-03-31 18:14:00 -0300

    rota api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'processos/index'
+    end
+  end
   root to: 'main_screen#index'
   get "/main_screen", to: "main_screen#index"
 
